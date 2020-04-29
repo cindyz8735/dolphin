480aedaebfaf8ab32f284565761324a29e4a93d7
Cindy Zhang <cindyz8735@gmail.com>
2020-04-29 00:36:20 -0700

    basic functionality and coloring

--- app.py
+++ app.py
@@ -3,13 +3,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def process_texts():
-    with open('1973_doubt_2.txt', 'r') as file:
-        text = file.read()
+def home_page():
+    return render_template('index.html')
 
+@app.route('/<file>')
+def display_poem(file):
+    with open(f'{file}.txt', 'r') as f:
+        full_text = f.read()
+
+    sources = []
+
+    components = full_text.split('NEW\nNEW\n')
+
+    for component in components:
+        text = component.split('\n', 2)
+        source = {'title': text[0],
+                  'poem_title': text[1],
+                  'stanzas': process_text(text[2])}
+        sources.append(source)
+
+    return render_template('poem.html', sources=sources, page_name=sources[0]['poem_title'])
+
+
+def process_text(text):
     stanzas = text.split('\n\n')
-    formatting = []
     currently_italics = False
+    diff_flag = 0
 
     res = []
 
@@ -21,17 +40,18 @@ def process_texts():
             words = line.split()
             res[i].append([])
 
-            for k, word in enumerate(words):
+            for word in words:
                 if word == '<begin_i>':
                     currently_italics = True
                 elif word == '<end_i>':
                     currently_italics = False
-
+                elif word.startswith('<begin_diff_'):
+                    diff_flag = word[-2]
+                elif word == '<end_diff>':
+                    diff_flag = 0
                 else:
-                    res[i][j].append((currently_italics, word))
-
-    return render_template('poem.html', source_title='The Dolphin, 1973',
-                           poem_title='Doubt 2: "Pointing the Horns of the Dilemma"', stanzas=res)
+                    res[i][j].append((currently_italics, diff_flag, word))
+    return res
 
 
 if __name__ == '__main__':
