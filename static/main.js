8ba36adda99b82d9a8df44c5123c179f7b83a1d7
Cindy Zhang <cindyz8735@gmail.com>
2020-05-05 18:16:06 -0700

    green sore, styling, buttons

--- static/main.js
+++ static/main.js
@@ -3,12 +3,23 @@ $(document).ready(function() {
     const num_colors = 5;
 
     $('#color-toggle').click(function() {
-        $('.diff').toggleClass('hide_colors');
+        if ($(this).text() === "Show All Colors") {
+            $('.diff').removeClass('hide_colors');
+        }
+        else {
+            $('.diff').addClass('hide_colors');
+        }
+        $(this).text(function(i, text){
+          return text === "Show All Colors" ? "Hide All Colors" : "Show All Colors";
+        })
     });
 
     $('.diff-button').click(function() {
         let diff_id = $(this).attr('diff');
-        let color_category = parseInt(diff_id) % num_colors;
-        $('.diff' + diff_id).toggleClass('color' + color_category);
+        $('.diff' + diff_id).toggleClass('hide_colors');
+        $()
+        $(this).text(function(i, text){
+          return text === "Show Difference " + diff_id ? "Hide Difference " + diff_id : "Show Difference " + diff_id;
+        })
     })
 });
